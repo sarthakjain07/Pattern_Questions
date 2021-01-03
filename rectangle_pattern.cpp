290cccd4a35ca80a054fe30808f7f6ffe2b65cc6
Sarthak Jain <sarthakjain7@outlook.com>
2021-01-03 08:09:25 +0530

    Added rectangle pattern program using c++

---
+++ rectangle_pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int rows,cols;
+    cout<<"Enter the number of rows and cloumns\n";
+    cin>>rows>>cols;
+    for (int i = 1; i <=rows; i++)
+    {
+        for (int j = 1; j <=cols; j++)
+            cout<<"*";
+        cout<<endl;    
+        
+    }
+    
+    return 0;
+}
