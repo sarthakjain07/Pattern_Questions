18b47b2225e3392939d8aae714c8c084cd9bd29b
Sarthak Jain <sarthakjain7@outlook.com>
2021-01-25 13:33:43 +0530

    Added program for half pyramid of numbers

---
+++ Numbers_half_pyramid.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int blocks;
+    cout<<"Enter the number of blocks\n";
+    cin>>blocks;
+    for (int i = 1; i <=blocks; i++)
+    {
+        for (int j = 1; j<=blocks; j++)
+        {
+            if(j<=i)
+                cout<<j;// We are printing columns here
+            else
+                cout<<" ";    
+        }    
+        cout<<endl;    
+    }
+    
+    return 0;
+
+}         
